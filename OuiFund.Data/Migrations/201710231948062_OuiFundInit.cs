d98c784460336e34a0636077d5346cce1bd018d9
wsouissi <wsouissi88@gmail.com>
2017-10-24 01:11:40 +0200

    Revert "Revert "Ajoutez des fichiers projet.""
    This reverts commit fe4566c5b7c8d447ef5f8e80e8cdec4a22db2ff7.

---
+++ OuiFund.Data/Migrations/201710231948062_OuiFundInit.cs
@@ -0,0 +1,26 @@
+namespace OuiFund.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class OuiFundInit : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserName = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+        }
+    }
+}
---
+++ OuiFund.Data/OuiFundContext.cs
@@ -0,0 +1,15 @@
+﻿using OuiFund.Domain;
+using System.Data.Entity;
+
+
+namespace OuiFund.Data
+{
+    public class OuiFundContext : DbContext
+    {
+        public OuiFundContext() : base("OuiFund")
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
